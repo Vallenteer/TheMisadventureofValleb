e8117de33aa934f9e84d5be8d46d1341a89f410e
Vallenteer <kristiantokelvin@yahoo.co.id>
2018-01-29 01:22:41 +0700

    Commit 114_2
    Change :+1:
    rubah sehingga listnya benar-benar harus ke TMII baru bisa akses PPIPTEK

---
+++ Assets/VisitListScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VisitListScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.GetInt("MUSEUM PPIPTEK TMIIPlayed") > 0)
+        {
+            GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
