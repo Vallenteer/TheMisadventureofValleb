3c380a68260e962f07833187af594439b6983ec3
Vallenteer <kristiantokelvin@yahoo.co.id>
2018-04-26 23:00:28 +0700

    How to play edit new

--- Assets/AppScript/HowToPlayMana.cs
+++ Assets/AppScript/HowToPlayMana.cs
@@ -6,34 +6,43 @@ using UnityEngine.UI;
 
 public class HowToPlayMana : MonoBehaviour {
     [Header("Question Holder")]
-    [SerializeField] Text questionHandler;
-
+    [SerializeField] Image questionHandler;
+    [SerializeField] GameObject prevButton;
 
     [Header("Question List")]
     [SerializeField]
-    string[] questionList;
+    Sprite[] questionList;
     int indexList;
 
 
     // Use this for initialization
     void Start () {
         indexList = 0;
-        questionHandler.text = questionList[indexList];
-        indexList++;
+        questionHandler.sprite = questionList[indexList];
+        //indexList++;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (indexList == 0)
+        {
+            prevButton.SetActive(false);
+        }
+        else {
+            prevButton.SetActive(true);
+        }
 		
 	}
     public void NextQuestion()
     {
         //cek apakah soal sudah habis atau belum
+        indexList++;
         if (indexList < questionList.Length)
         {
             //reset all parameter and move to next question
-            questionHandler.text = questionList[indexList];
-            indexList++;
+            
+            questionHandler.sprite = questionList[indexList];
+            
             
         }
         else
@@ -42,5 +51,17 @@ public class HowToPlayMana : MonoBehaviour {
             SceneManager.LoadScene(0);
         }
 
+    }
+    public void PrevQuestion()
+    {
+        //cek apakah soal sudah habis atau belum
+        
+        if (indexList > 0)
+        {
+            //reset all parameter and move to next question
+            indexList--;
+            questionHandler.sprite = questionList[indexList];
+            
+        }
     }
 }
