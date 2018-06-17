c0664ec3a0ce702a6001bfc741954ce5d0daaf64
Vallenteer <kristiantokelvin@yahoo.co.id>
2018-06-18 01:27:22 +0700

    not working...
    baca Json nya gk bsa,
    gk bsa di parsing
    baca bisa tp gk bsa parsing

---
+++ Assets/AppScript/DataJsonLoad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DataJsonLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GetPertanyaan());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator GetPertanyaan()
+    {
+        string getPertanyaanUrl = "http://museumadv.azurewebsites.net/museum/list";
+        using (UnityWebRequest www = UnityWebRequest.Post(getPertanyaanUrl,"1"))
+        {
+            //www.chunkedTransfer = false;
+            yield return www.Send();
+            if (www.isError || www.responseCode==500 || www.responseCode==404)
+            {
+                Debug.Log(www.responseCode);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    string jsonResult =System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    Debug.Log(jsonResult);
+
+                    //JPertanyaan[] entities = JsonHelper.getJsonArray<JPertanyaan>(jsonResult);
+                    JMuseum[] entities = JsonHelper.getJsonArray<JMuseum>(jsonResult);
+
+
+                    foreach (var pert in entities)
+                    {
+                        Debug.Log(pert.museum);
+                    }
+
+                }
+                
+            }
+        }
+    }
+
+}
---
+++ Assets/AppScript/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
+
+[System.Serializable]
+public class JPertanyaan
+{
+    public int id { get; set; }
+    public string soal { get; set; }
+    public string jawaban { get; set; }
+    public string museum { get; set; }
+    public string petunjuk { get; set; }
+}
+[System.Serializable]
+public class JMuseum
+{
+    public int id { get; set; }
+    public string museum { get; set; }
+}
+[System.Serializable]
+public class JVersion
+{
+    public double version { get; set; }
+}
